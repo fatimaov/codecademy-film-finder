840705a4b675723a6db3c9b4cd66705a34634fe1
Fatima Olea <232623523+fatimaov@users.noreply.github.com>
2026-02-05 20:17:42 +0100

    Get a random movie

--- script.js
+++ script.js
@@ -21,8 +21,22 @@ const getGenres = async () => {
     }
 };
 
-const getMovies = () => {
+const getMovies = async () => {
   const selectedGenre = getSelectedGenre();
+  const discoverMovieEndPoint = '/discover/movie';
+  const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}`;
+  const urlToFetch = tmbdBaseUrl+discoverMovieEndPoint+requestParams;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+        const jsonResponse = await response.json();
+        const movies = jsonResponse.results;
+        return movies;
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
 
 };
 
